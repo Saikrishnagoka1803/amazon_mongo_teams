0367d5618e736df413d67f8c2acce6ff26b7e431
Martyna <martyna.sowinska@hotmail.com>
2021-10-08 16:24:48 +0200

    Cleaning cart layout

--- src/services/cart/schema.js
+++ src/services/cart/schema.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const cartSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
-    status: { type: String, enum: ["active", "paid"], default: "active" },
-    products: [{ title: String, price: Number, quantity: Number }],
-  })
-  
-  export default model("Cart", cartSchema)
+  userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
+  status: { type: String, enum: ["active", "paid"], default: "active" },
+  products: [{ title: String, price: Number, quantity: Number }],
+})
+
+export default model("Cart", cartSchema)
